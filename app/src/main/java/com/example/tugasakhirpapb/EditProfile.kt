5d6dbd7d6341a345d91e0ab610d0273a11f99949
USER <ayupuspita@student.ub.ac.id>
2021-11-23 23:08:45 +0700

    add profile desain

--- app/src/main/java/com/example/tugasakhirpapb/EditProfile.kt
+++ app/src/main/java/com/example/tugasakhirpapb/EditProfile.kt
@@ -3,7 +3,7 @@ package com.example.tugasakhirpapb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
--- app/src/main/java/com/example/tugasakhirpapb/Profile.kt
+++ app/src/main/java/com/example/tugasakhirpapb/Profile.kt
@@ -3,9 +3,9 @@ package com.example.tugasakhirpapb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        setContentView(R.layout.activity_profile)
     }
 }
