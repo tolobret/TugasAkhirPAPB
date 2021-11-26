34adb884f10871e878be20cc9ee6d040ee594a48
achmad.nofandi <achmad.nofandi@gmail.com>
2021-11-26 21:33:13 +0700

    update HalamanKonten

---
+++ app/src/main/java/com/example/tugasakhirpapb/HalamanKonten.kt
@@ -0,0 +1,11 @@
+package com.example.tugasakhirpapb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanKonten : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_konten)
+    }
+}
