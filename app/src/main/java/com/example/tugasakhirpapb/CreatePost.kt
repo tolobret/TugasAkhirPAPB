e7efe99f1921c3301a8b3924df8684569010212b
achmad.nofandi <achmad.nofandi@gmail.com>
2021-11-28 17:07:24 +0700

    createpost-database OK

--- app/src/main/java/com/example/tugasakhirpapb/CreatePost.kt
+++ app/src/main/java/com/example/tugasakhirpapb/CreatePost.kt
@@ -2,10 +2,47 @@ package com.example.tugasakhirpapb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.tugasakhirpapb.Model.Konten
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class CreatePost : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    lateinit var textJudul : EditText
+    lateinit var textKonten : EditText
+    lateinit var textLocation : EditText
+    lateinit var btPost : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        textJudul = findViewById(R.id.text_Judul)
+        textKonten = findViewById(R.id.text_Post)
+        textLocation = findViewById(R.id.text_Location)
+        btPost = findViewById(R.id.bt_post)
+
+
+        database = FirebaseDatabase.getInstance().getReference()
+
+
+
+        btPost.setOnClickListener(){
+            val konten = Konten(
+                textJudul.text.toString(),
+                textKonten.text.toString(),
+                textLocation.text.toString()
+            )
+            database.child("konten").child(textJudul.text.toString()).setValue(konten).addOnSuccessListener {
+                Toast.makeText(this,"Successfully Posted", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Toast.makeText(this,"Failed to Post",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/tugasakhirpapb/Model/Konten.kt
@@ -0,0 +1,13 @@
+package com.example.tugasakhirpapb.Model
+
+
+data class Konten(val judul : String? = null,
+                  val konten_cerita : String? = null,
+                  val location : String? = null,
+                  val userName : String? = null,
+                  val foto : String? = null,
+                  val like_count : Int? = 0,
+                  val created_date : String? = null){
+
+
+}
