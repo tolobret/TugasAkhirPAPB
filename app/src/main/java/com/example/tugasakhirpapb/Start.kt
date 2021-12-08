f67462382071722cb78d9eb6790e65ae0d682627
ZahraMaharani <zahraamaharani@student.ub.ac.id>
2021-12-08 16:48:36 +0700

    start

--- app/src/main/java/com/example/tugasakhirpapb/Start.kt
+++ app/src/main/java/com/example/tugasakhirpapb/Start.kt
@@ -1,4 +1,25 @@
 package com.example.tugasakhirpapb
 
-class Start {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Start : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start)
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        val btn_signup = findViewById<Button>(R.id.btn_signup)
+
+        btn_login.setOnClickListener {
+            val go = Intent(this@Start, Login::class.java)
+            startActivity(go)
+        }
+
+        btn_signup.setOnClickListener {
+            val go = Intent(this@Start, Register::class.java)
+            startActivity(go)
+        }
+    }
 }
